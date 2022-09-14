d879565c2f1560180a70436bc385e7dac7002b0b
Skarabela <martin.skarabela@chyron.com>
2022-09-15 01:32:27 +0200

    Vitkovice wordpress theme >> repair of search form

--- vitkovice-wp-theme/searchform.php
+++ vitkovice-wp-theme/searchform.php
@@ -1,4 +1,4 @@
-<form class="mt-3" role="search" method="get" action="<?php echo home_url('/'); ?>">
+<form class="mt-4 mt-lg-0" role="search" method="get" action="<?php echo home_url('/'); ?>">
     <div class="input-group">
         <input class="form-control" type="search" placeholder="Hledat" aria-label="Hledat" name="s" id="s" value="<?php the_search_query(); ?>"><!--TODO-->
         <button class="btn btn-outline-primary" type="submit">Hledat</button><!--TODO-->
