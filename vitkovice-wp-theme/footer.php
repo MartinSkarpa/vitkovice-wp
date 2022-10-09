9d4addfcc7533070233d1f9901f0a92d835bf383
Skarabela <martin.skarabela@chyron.com>
2022-10-10 01:14:57 +0200

    Vitkovice wordpress theme >> repaired Bootstrap problem

--- vitkovice-wp-theme/footer.php
+++ vitkovice-wp-theme/footer.php
@@ -1,5 +1,5 @@
 <?php
-    wp_enqueue_script("bootstrap.min", get_theme_root_uri()._ROOT_DIR."/js/bootstrap/bootstrap.min.js");
+    wp_enqueue_script("bootstrap.bundle.min", get_theme_root_uri()._ROOT_DIR."/js/bootstrap/bootstrap.bundle.min.js");
 
     wp_footer();
 ?>
